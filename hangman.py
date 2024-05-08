0fff817bd9e06b8f2c019313371f6a717026277a
AliVal1yev <eliveliyev150@gmail.com>
2024-05-08 15:21:29 +0400

    String has been imported and backgraund color added

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import os
+import string
 import random
 from faker import Faker
 
@@ -7,18 +8,32 @@ fake = Faker()
 
 data = {
     "first name": [fake.first_name() for _ in range(5)],
-    "city name": [fake.city() for _ in range(5)],
+    "city": [fake.city() for _ in range(5)],
     "country": [fake.country() for _ in range(5)]
 }
+
+lett = string.ascii_lowercase
+letters = list(lett)
+print(letters)
 
 category, words = random.choice(list(data.items()))
 word = random.choice(words)
 word = f"{word}".lower()
+word = word.replace(' ', '')
 random_index = random.randint(0, len(word) - 1)
 sep_word = list(word)                                             
 masked_word = ["_" for _ in word] 
 masked_word[random_index] = word[random_index]
-CHANCES = 3
+
+CHANCES = 5
+RED_PREFIX = "\033[41m"
+GREEN_PREFIX = "\033[42m"
+COLOR_SUFFIX = "\033[0m"
+
+color_data = {
+    "red": RED_PREFIX,
+    "green": GREEN_PREFIX
+}
 
 death_road = {
     3: r"""
@@ -56,27 +71,36 @@ death_road = {
 
 while CHANCES > 0:
     os.system("cls")
-    print(death_road[CHANCES])
+    #print(death_road[CHANCES])
     print(f"Category is '{category}'. You have {CHANCES} chances. Good luck!")
     print("".join(masked_word))
+    for letter in letters:
+        print(letter, end=" ")
+    print()
     let = input(">> ")
-
+    
     if not let:
         continue
 
     if let in word:
         c = word.count(let)
+        
         for _ in range(c):
             try:
                 idx = sep_word.index(let)
                 masked_word[idx] = let
                 sep_word[idx] = "*"
+                indx_string = letters.index(let)
+                letters[indx_string] = f"{color_data['green']}{let}{COLOR_SUFFIX}"
+                
             except ValueError:
                 input("You already input this letter. Try another one. :))")
                 continue
         print("".join(masked_word))
     else:
         CHANCES -= 1
+        indx_string = letters.index(let)
+        letters[indx_string] = f"{color_data['red']}{let}{COLOR_SUFFIX}"
     guessed = all([ch=="*" for ch in sep_word])
 
     if guessed:
